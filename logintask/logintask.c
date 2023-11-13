b168e1ebbe2f228c6f294a7f4c2b84608e4f7bfb
Your Name <you@example.com>
2023-11-13 21:17:29 +0200

    task2

--- logintask/logintask.c
+++ logintask/logintask.c
@@ -1,46 +1,31 @@
 
 #include<stdio.h>
-#include<stdbool.h>
 int main(){
   char pass[30];
   char name[50];
   char loginName[50];
   char loginpass[30];
- char flag[10];
-    
     printf("Registeration Form : \n");
     printf("Enter your Name : ");
     scanf("%s",&name);
     printf("Enter your password : ");
     scanf("%s",&pass);
-     printf("Enter true or 1 : ");
-    scanf("%s",&flag);
-    //char x="true"; 
-    //sschar y="1";
-      if(strcmp(flag,"true")==0||strcmp(flag,"1")==0){
-    
+    //***********************
     printf("Now Try login : \n");
-    
+    //***********************
     int state=1;
     do{
-
     printf("Name : ");
     scanf("%s",&loginName);
     printf("Password : ");
     scanf("%s",&loginpass);
    
     if(strcmp(loginName,name)==0&&strcmp(loginpass,pass)==0) 
-    { 
-      
-      printf("Login Succesfuly");
+    { printf("Login Succesfuly");
     state=0;}                                                              
     else{
         printf("Incorrect Name or Password Try Again \n");
-    }}while (state==1);}
-    //******************
-    else{
-printf("please Enter true or 1\n");
-    }
+    }}while (state==1);
     //*****************
     return 0;
 }
